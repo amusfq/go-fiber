3d22abdcc77f37570d02c3d2523638936a93cac5
Achmad Musyaffa Taufiqi <amusfq@gmail.com>
2023-03-31 11:28:23 +0700

    Done

--- handlers/todos.go
+++ handlers/todos.go
@@ -17,7 +17,15 @@ func GetAllTodo(c *fiber.Ctx) error {
 }
 
 func GetTodo(c *fiber.Ctx) error {
-	return c.SendString("Div Rhino Trivia App!")
+	id := c.Params("id")
+	data := models.Todo{}
+	result := database.DB.Db.Where("id = ?", id).First(&data)
+	if result.Error != nil {
+		return c.Status(404).SendString("Not Found")
+	}
+	return c.Render("todo/view", fiber.Map{
+		"Data": data,
+	})
 }
 
 func CreateTodoView(c *fiber.Ctx) error {
@@ -38,4 +46,36 @@ func CreateTodo(c *fiber.Ctx) error {
 
 	database.DB.Db.Create(&data)
 	return c.Render("todo/success", fiber.Map{})
+}
+
+func UpdateTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+	data := new(models.Todo)
+
+	if err := c.BodyParser(data); err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"Message": err.Error(),
+		})
+	}
+	if data.Title == "" {
+		return c.Status(500).JSON(fiber.Map{"Message": "Title is required"})
+	}
+	result := database.DB.Db.Model(&data).Where("id = ?", id).Updates(data)
+	if result.Error != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"Message": result.Error,
+		})
+	}
+	return c.Status(200).JSON(fiber.Map{
+		"Message": "Success",
+	})
+}
+
+func DeleteTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+	data := models.Todo{}
+	database.DB.Db.Delete(&data, id)
+	return c.Status(200).JSON(fiber.Map{
+		"message": "Success",
+	})
 }
--- router/router.go
+++ router/router.go
@@ -10,6 +10,8 @@ func SetupRoutes(app *fiber.App) {
 	todo := app.Group("todo")
 	todo.Get("/create", handlers.CreateTodoView)
 	todo.Get("/:id", handlers.GetTodo)
+	todo.Delete("/:id", handlers.DeleteTodo)
+	todo.Patch("/:id", handlers.UpdateTodo)
 	todo.Get("/", handlers.GetAllTodo)
 	todo.Post("/", handlers.CreateTodo)
 }
